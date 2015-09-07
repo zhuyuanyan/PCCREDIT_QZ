c0d1183115be066c62570e2237c26f77ab268e7f
chinhBy <chinenghe2010@163.com>
2015-09-07 12:40:15 +0800

    [add]最新

---
+++ src/java/com/cardpay/pccredit/intopieces/filter/QuotaFreezeOrThawFilter.java
@@ -0,0 +1,58 @@
+package com.cardpay.pccredit.intopieces.filter;
+
+import com.wicresoft.jrad.base.database.dao.business.BusinessFilter;
+
+public class QuotaFreezeOrThawFilter extends BusinessFilter{
+	
+	private String customerName;
+	
+	private String certNo;
+	
+	private String clientNo;
+	
+	private String loanStatus;
+	
+	private String userId;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCertNo() {
+		return certNo;
+	}
+
+	public void setCertNo(String certNo) {
+		this.certNo = certNo;
+	}
+
+	public String getClientNo() {
+		return clientNo;
+	}
+
+	public void setClientNo(String clientNo) {
+		this.clientNo = clientNo;
+	}
+
+	public String getLoanStatus() {
+		return loanStatus;
+	}
+
+	public void setLoanStatus(String loanStatus) {
+		this.loanStatus = loanStatus;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+}
